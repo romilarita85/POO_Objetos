82b123e51409652a17732661cc4d6301547f2c6a
romilarita85 <romico_85@outlook.com.ar>
2024-09-22 09:55:30 -0300

    repaso poo

---
+++ RominaCompara/clase19_09/Auto.cs
@@ -0,0 +1,69 @@
+﻿//Encapsulamiento: forma de restringir el acceso total o parcial de nuestros atributos o metodos dentro de la clase
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase19_09
+{
+    //visibilidad de la clase: siempre publica
+    //Clases de instancia
+    public class Auto // si no dice static la clase es de INSTANCIA
+    {
+        private string patente;
+        private int cantidadRuedas;
+        private string tipo;
+        private double cantidadCombustible;
+        private Color color;
+        
+        //METODOS SETTERS Y GETTERS
+        public void SetPatente(string patente) 
+        {
+            this.patente = patente;
+        }
+        //this: para saber a que hago referencia. Identifica cual es la instancia
+        public void SetCantRuedas(int cant) 
+        { 
+            this.cantidadRuedas = cant;
+        }
+        public void SetTipo(string tipo) 
+        {
+            this.tipo = tipo;   
+        }
+        public bool SetCantCombustible(double cant) 
+        {//validaciones dentro del set
+            bool ok = false; //por defecto
+            if (cant > 0) 
+            {
+                this.cantidadCombustible = cant; 
+                ok = true;
+            }
+            return ok;
+        }
+        public void SetColor(Color color)
+        {
+            this.color = color; 
+        } 
+
+        //Metodos -> funciones
+        //Visibilidad-comportamiento-retorno-nombre-parametros(opcional)
+        public bool Conducir(double distancia) 
+        {//por cada litro de combustible yo puedo hacer cierta distancia
+            bool sePudo = false;//por defecto lo inicializo en false
+            double consumo = distancia / 4;//por cada litro puedo hacer 4 kilometros
+
+            if (cantidadCombustible > consumo) 
+            {
+                sePudo = true;
+                cantidadCombustible -= consumo;
+            }
+            return sePudo;//retorno
+        }
+        public string MostrarInformacion() 
+        {
+            return $"Patente: {patente} - Tipo: {tipo} - Color: {color.Name} - Cantidad de ruedad: {cantidadRuedas} - Cantidad de combustible {cantidadCombustible}";
+        }
+    }
+}
--- RominaCompara/clase19_09/Program.cs
+++ RominaCompara/clase19_09/Program.cs
@@ -1,10 +1,101 @@
-﻿namespace clase19_09
+﻿
+using clase19_09;
+using System.Drawing;
+
+namespace clase19_09
 {
     internal class Program
     {
+        //La clase main por defecto es estatica
         static void Main(string[] args)
-        {
+        {                    //constructor
+            Auto auto1 = new Auto();
             
+            //Variables definidas dentro del main
+            string patente = "AA456CV";//inicialiar la variable
+            int cantidadRuedas = 4;
+            string tipo = "Sedan";
+            double cantidadCombustible = 0;
+            Color color = Color.DarkGreen;
+            
+            auto1.SetPatente(patente);
+            auto1.SetTipo(tipo);
+            auto1.SetCantRuedas(cantidadRuedas);
+
+            if (auto1.SetCantCombustible(cantidadCombustible))
+            {
+                Console.WriteLine("Se pudo ingresar la cantidad de combustible");
+            }
+            else 
+            {
+                Console.WriteLine("El valo de cant de combustible es incorrecto");
+            }
+            auto1.SetColor(color);
+
+            Console.WriteLine(auto1.MostrarInformacion());
+
+           
         }
     }
 }
+//-----------------------------------
+//static void Main(string[] args)
+//{                    //constructor
+//    Auto auto1 = new Auto();
+//    Console.WriteLine(auto1.MostrarInformacion());
+
+//    auto1.patente = "AA456CV";
+//    auto1.color = Color.DarkMagenta;
+//    auto1.cantidadRuedas = 4;
+//    auto1.cantidadCombustible = 70;
+//    auto1.tipo = "Sedan";
+//    Console.WriteLine(auto1.MostrarInformacion());
+
+//    
+//}
+//--------------------------------------
+//static void Main(string[] args)
+//{                    //constructor
+//    Auto auto1 = new Auto();
+
+//    //Variables definidas dentro del main
+//    string patente = string.Empty;//inicialiar la variable
+//    int cantidadRuedas = 0;
+//    string tipo = string.Empty;
+//    double cantidadCombustible = 0;
+//    Color color = Color.DarkGreen;
+
+//    //paso las variables a mi auto
+//    auto1.patente = patente;
+//    auto1.color = color;
+//    auto1.cantidadRuedas = cantidadRuedas;
+//    auto1.cantidadCombustible = cantidadCombustible;
+//    auto1.tipo = tipo;
+//    Console.WriteLine(auto1.MostrarInformacion());
+
+
+//}
+//---------------------------------------------
+//static void Main(string[] args)
+//{                    //constructor
+//    Auto auto1 = new Auto();
+
+//    //Variables definidas dentro del main
+//    string patente = "AA456CV";//inicialiar la variable
+//    int cantidadRuedas = 4;
+//    string tipo = "Sedan";
+//    double cantidadCombustible = 70;
+//    Color color = Color.DarkGreen;
+
+//    //paso las variables a mi auto
+//    auto1.patente = patente;
+//    auto1.color = color;
+//    auto1.cantidadRuedas = cantidadRuedas;
+//    auto1.cantidadCombustible = cantidadCombustible;
+//    auto1.tipo = tipo;
+//    Console.WriteLine(auto1.MostrarInformacion());
+
+//    auto1.patente = "FD123WS"; //cualquier usuario puede modificar
+//                               //no esta protegido, no hay encapsulamiento
+//    Console.WriteLine(auto1.MostrarInformacion());
+//}
